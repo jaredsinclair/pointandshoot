cf03d45e92e47b1a10d6c38549e704422c840801
Jared Sinclair <jaredsinclair.rn@gmail.com>
2020-01-02 16:36:00 -0600

    Add sample project files.

---
+++ SampleProject/SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Jared Sinclair on 1/2/20.
+//  Copyright © 2020 Nice Boy, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

c9ebe7ff72c0065e65ba3802a0502390d7ab6c56
Jared Sinclair <jaredsinclair.rn@gmail.com>
2020-01-03 23:11:01 -0600

    Point dependencies to master.

--- Package.swift
+++ Package.swift
@@ -2,11 +2,6 @@
 
 import PackageDescription
 
-// @FIXME: Change this to a `conditional:` argument when that feature is
-// implemented by SPM and incorporated into Xcode:
-let etceteraBranchName = "develop"
-// Proposal: https://github.com/apple/swift-evolution/blob/master/proposals/0273-swiftpm-conditional-target-dependencies.md
-
 let package = Package(
     name: "PointAndShoot",
     platforms: [
@@ -18,7 +13,7 @@ let package = Package(
             targets: ["PointAndShoot"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/jaredsinclair/etcetera.git", .branch(etceteraBranchName)),
+        .package(url: "https://github.com/jaredsinclair/etcetera.git", .branch("master")),
         .package(url: "https://github.com/jaredsinclair/OrientationObserver", .branch("master")),
     ],
     targets: [
